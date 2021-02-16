4a6f46809a3ca50c1afee97ff2a4f21cfa5813a8
JamesDMSpeed <james.speed@ntnu.no>
2021-02-16 11:20:23 +0100

    Formatting event date

--- R scripts/Troendelag species list.R
+++ R scripts/Troendelag species list.R
@@ -7,24 +7,31 @@ library(taxize)
 sustherbveg_url<-'https://ntnu.box.com/shared/static/zrwchyp1sba9pqubbf47eyc3z2pvp3xg.txt'
 download.file(sustherbveg_url,'VegetationData/SustHerbVegetation.txt')
 sustherbveg<-read.table('VegetationData/SustHerbVegetation.txt',header=T,sep='\t')
-
-
 str(sustherbveg)
+
 #Find species list in Trøndelag
-pmatfun<-function(x, table) pmatch(x, table, nomatch = 0) >0
 trondveg<-sustherbveg[grepl('Trøndelag',sustherbveg$locality),]
-trondveg
+trondveg<-droplevels(trondveg)
 
 levels(sustherbveg$scientificName)
 levels(droplevels(trondveg$scientificName))
 
+#Convert date to date format
+trondveg$eventDate<-as.Date(as.character(trondveg$eventDate),tryFormats = c('%d.%m.%Y'))
+range(trondveg$eventDate)
+
+# Species list ------------------------------------------------------------
 #List of Trøndelag species
 splist<-levels(droplevels(trondveg$scientificName))
 
+
 #Check these
 splist_resolved<-gnr_resolve(splist,best_match_only = T)
-splist_checked<-splist
+splist_resolved
+attributes(splist_resolved)$not_known#Species not found (not species)
 
 #Get classes
-tax_name(splist_resolved$matched_name,get=c("class"),db='ncbi')
+spclasses<-tax_name(splist_resolved$matched_name,get=c("class"),db='ncbi')
+
+write.csv(spclasses,'VegetationData/SpeciesfromTroendelag.csv')
 
